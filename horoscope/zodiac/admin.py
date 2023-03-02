0d1b13de62a17e256b3d819ed53110a874345a65
Yaroslav Galkin <galkinyaroslav@gmail.com>
2023-03-02 15:13:04 +0300

    add admin models, add slug to models

--- horoscope/zodiac/admin.py
+++ horoscope/zodiac/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ZodiacSigns, SignHoroscope
+
 
 # Register your models here.
+
+class ZodiacSignsAdmin(admin.ModelAdmin):
+    list_display = ('sign_name', 'from_date', 'to_date',)
+    prepopulated_fields = {'slug': ('sign_name',)}
+
+admin.site.register(ZodiacSigns,ZodiacSignsAdmin)
+
+class SignHoroscopeAdmin(admin.ModelAdmin):
+    list_display = ('sign_horoscope', 'sign', 'date',)
+    prepopulated_fields = {'slug': ('date',)}
+
+admin.site.register(SignHoroscope,SignHoroscopeAdmin)
--- horoscope/zodiac/models.py
+++ horoscope/zodiac/models.py
@@ -4,28 +4,21 @@ from django.db import models
 
 # Create your models here.
 
-class Zodiac(models.Model):
-    aries = models.TextField()  # (Ram): #March 21–April 19
-    taurus = models.TextField()  # (Bull): #April 20–May 20
-    gemini = models.TextField()  # (Twins): #May 21–June 21
-    cancer = models.TextField()  # (Crab): #June 22–July 22
-    leo = models.TextField()  # (Lion): #July 23–August 22
-    virgo = models.TextField()  # (Virgin): #August 23–September 22
-    libra = models.TextField()  # (Balance): #September 23–October 23
-    scorpius = models.TextField()  # (Scorpion): #October 24–November 21
-    sagittarius = models.TextField()  # (Archer): #November 22–December 21
-    capricornus = models.TextField()  # (Goat): #December 22–January 19
-    aquarius = models.TextField()  # (Water Bearer): #January 20–February 18
-    pisces = models.TextField()  # (Fish): February 19–March 20
-
-    month = models.IntegerField(
-        default=1,
-        validators=[
-            MaxValueValidator(12),
-            MinValueValidator(1)
-        ]
-     )
-
+class ZodiacSigns(models.Model):
+    sign_name = models.CharField(max_length=11)
+    from_date = models.CharField(max_length=30)
+    to_date = models.CharField(max_length=30)
+    slug = models.SlugField(default='', null=False)
 
     def __str__(self):
-        return f''
+        return f'{self.sign_name}: {self.from_date}-{self.to_date}'
+
+
+class SignHoroscope(models.Model):
+    sign_horoscope = models.TextField()
+    sign = models.ForeignKey(ZodiacSigns, on_delete=models.CASCADE)
+    date = models.DateField()
+    slug = models.SlugField(default='',null=False)
+
+    def __str__(self):
+        return f'Horoscope for {self.sign} on {self.date}'
